c5dfef297696f98fd532a4804f6cabb734586721
王天翼 <wangtianyi@4paradigm.com>
2025-06-26 18:36:46 +0800

    基本功能完成

--- exercises/ch-3-ex-1/client.js
+++ exercises/ch-3-ex-1/client.js
@@ -6,6 +6,7 @@ var querystring = require('querystring');
 var cons = require('consolidate');
 var randomstring = require("randomstring");
 var __ = require('underscore');
+const { log } = require("console");
 __.string = require('underscore.string');
 
 var app = express();
@@ -27,8 +28,8 @@ var authServer = {
  * Add the client information in here
  */
 var client = {
-	"client_id": "",
-	"client_secret": "",
+	"client_id": "oauth-client-1",
+	"client_secret": "oauth-client-secret-1",
 	"redirect_uris": ["http://localhost:9000/callback"]
 };
 
@@ -44,26 +45,74 @@ app.get('/', function (req, res) {
 });
 
 app.get('/authorize', function(req, res){
-
-	/*
-	 * Send the user to the authorization server
-	 */
-	
+	var buildUrl = function(base, options, hash){
+		var newUrl = url.parse(base,true);
+		delete newUrl.search;
+		if (!newUrl.query) {
+			newUrl.query = {};
+		}
+		__.each(options,function(value, key, list) {
+			newUrl.query[key] = value;
+		});
+		if (hash) {
+			newUrl.hash = hash;
+		}
+		return url.format(newUrl);
+	};
+	state = randomstring.generate(16);
+	var authorizeUrl = buildUrl(authServer.authorizationEndpoint, {
+		response_type: 'code',
+		client_id: client.client_id,
+		redirect_uri: client.redirect_uris[0],
+		state: state
+	});
+	res.redirect(authorizeUrl);
 });
 
 app.get('/callback', function(req, res){
-
-	/*
-	 * Parse the response from the authorization server and get a token
-	 */
-	
+	var code = req.query.code;
+	var form_data = qs.stringify({
+		grant_type: 'authorization_code',
+		code: code,
+		redirect_uri: client.redirect_uris[0],
+		state: state
+	});
+	var headers = {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Authorization': 'Basic ' + encodeClientCredentials(client.client_id, client.client_secret)
+	}
+	var tokRes = request('POST', authServer.tokenEndpoint, {
+		body: form_data,
+		headers: headers
+	});
+	var body = JSON.parse(tokRes.getBody('utf8'));
+	log('Token response: ' + JSON.stringify(body));
+	access_token = body.access_token;
+	log('set access_token: ' + access_token);
+	res.render('index',{access_token: body.access_token});
 });
 
 app.get('/fetch_resource', function(req, res) {
-
-	/*
-	 * Use the access token to call the resource server
-	 */
+	if (!access_token){
+		res.render('error',{error: 'Missing access token'});
+		return;
+	}
+	var headers = {
+		'Authorization': 'Bearer ' + access_token
+	};
+	var resource = request('POST', protectedResource, {
+		headers: headers
+	});
+	log('Resource response: ' + resource.statusCode);
+	if (resource.statusCode >= 200 && resource.statusCode < 300) {
+		var body = JSON.parse(resource.getBody('utf8'));
+		res.render('data',{resource: body});
+		return;
+	}else{
+		log('Error fetching resource: ' + resource.statusCode);
+		res.render('error',{error: 'Error fetching resource: ' + resource.statusCode});
+		return;
+	}
 	
 });
 
