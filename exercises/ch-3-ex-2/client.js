faa8d30a6ed912f06ef6eedacc42ff5c61da2be3
王天翼 <wangtianyi@4paradigm.com>
2025-06-26 18:59:20 +0800

    save

--- exercises/ch-3-ex-2/client.js
+++ exercises/ch-3-ex-2/client.js
@@ -6,6 +6,8 @@ var querystring = require('querystring');
 var cons = require('consolidate');
 var randomstring = require("randomstring");
 var __ = require('underscore');
+const { ref } = require("process");
+const { log } = require("console");
 __.string = require('underscore.string');
 
 
@@ -136,19 +138,43 @@ app.get('/fetch_resource', function(req, res) {
 		/*
 		 * Instead of always returning an error like we do here, refresh the access token if we have a refresh token
 		 */
+		access_token = null;
+		if (refresh_token) {
+			refreshAccessToken(req,res);
+			//res.redirect('/fetch_resource');
+			return;
+		}else{
 		console.log("resource status error code " + resource.statusCode);
 		res.render('error', {error: 'Unable to fetch resource. Status ' + resource.statusCode});
+		}
 	}
 	
 	
 });
 
 var refreshAccessToken = function(req, res) {
-
-	/*
-	 * Use the refresh token to get a new access token
-	 */
-	
+	var form_data = qs.stringify({
+		grant_type: 'refresh_token',
+		refresh_token: refresh_token
+	});
+	var headers = {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Authorization': 'Basic ' + encodeClientCredentials(client.client_id, client.client_secret)
+	};
+	var tokRes = request('POST', authServer.tokenEndpoint, 
+		{	
+			body: form_data,
+			headers: headers
+		}
+	);
+	log("response:",tokRes);
+	log("body:",tokRes.getBody('utf8'));
+	body = tokRes.getBody('utf8');
+	access_token = body.access_token;
+	if (body.refresh_token){
+		refresh_token = body.refresh_token;
+	}
+	res.redirect('/fetch_resource');
 };
 
 var buildUrl = function(base, options, hash) {
